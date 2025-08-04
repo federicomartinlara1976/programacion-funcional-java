89283f28c0c05ecd66ace0aaf7934876fbb2b29c
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-04 09:19:50 +0200

    Fixed bugs

--- InterfazBiConsumerTest/src/main/java/ejercicios/InterfazBiConsumerTest.java
+++ InterfazBiConsumerTest/src/main/java/ejercicios/InterfazBiConsumerTest.java
@@ -2,11 +2,14 @@ package ejercicios;
 
 import java.util.function.BiConsumer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class InterfazBiConsumerTest {
     public static void main(String[] args) {
-        BiConsumer<String, Integer> biConsumer = (num1, num2) -> {
+        BiConsumer<Double, Double> biConsumer = (num1, num2) -> {
             double mult = num1 * num2;
-            System.out.println("La multiplicación de " + num1 + " y " + num2 + " es: " + mult);
+            log.info("La multiplicación de {} y {} es {}", num1, num2, mult);
         };
         biConsumer.accept(15.2, 52.25);
     }
--- InterfazBiConsumerTest/src/main/java/ejercicios/MapTest.java
+++ InterfazBiConsumerTest/src/main/java/ejercicios/MapTest.java
@@ -1,13 +1,15 @@
 package ejercicios;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Iterator;
+import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MapTest {
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         String[][] arreglo = {{"Chris", "USA"}, {"Raju", "India"}, {"Lynda", "Canada"}};
 
         // Crear mapa usando el array
@@ -15,18 +17,18 @@ public class MapTest {
             map.put(arreglo[i][0], arreglo[i][1]);
         }
 
-        Imprimir mapa forma tradicional
-        System.out.println("Usando la forma tradicional de imprimir clave y valor de un mapa:");
-        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+        //Imprimir mapa forma tradicional
+        log.info("Usando la forma tradicional de imprimir clave y valor de un mapa:");
+        Iterator<Map.Entry<String, String>> iter = map.entrySet().iterator();
         if(iter!=null) {
             while(iter.hasNext()) {
-                Map.Entry<String, Integer> entry = iter.next();
-                System.out.println("Clave: " + entry.getKey() + " - Valor: " + entry.getValue());
+                Map.Entry<String, String> entry = iter.next();
+                log.info("Clave: {} - Valor: {}", entry.getKey(), entry.getValue());
             }
         }
 
         // Imprimir mapa usando la forma moderna de imprimir clave y valor de un mapa
-        System.out.println("Usando BiConsumer y expresion lambda:");
-        map.forEach((key, value) -> System.out.println("Clave: " + key + " - Valor: " + value));
+        log.info("Usando BiConsumer y expresion lambda:");
+        map.forEach((key, value) -> log.info("Clave: {} - Valor: {}", key, value));
     }
 }
