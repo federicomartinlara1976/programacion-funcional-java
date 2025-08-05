eb43c48649fd5354bd56d07bd65222c0231870a0
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-05 11:31:55 +0200

    add more tests

--- InterfazFunctionTest/src/main/java/ejerciciofunction/TestFunction.java
+++ InterfazFunctionTest/src/main/java/ejerciciofunction/TestFunction.java
@@ -1,16 +1,25 @@
 package ejerciciofunction;
 
 import java.util.function.Function;
+import java.util.function.IntFunction;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TestFunction {
     public static void main(String[] args) {
-        Function<Integer, String> convertidor = x -> Integer.toString(x);
+    	log.info("Con interfaz Function");
+    	Function<Integer, String> convertidor = x -> Integer.toString(x);
         
         log.info("{}", convertidor.apply(3).length());
         log.info("{}", convertidor.apply(30).length());
         log.info("{}", convertidor.apply(300).length());
+        
+        log.info("Con interfaz IntFunction");
+    	IntFunction<String> convertidor2 = x -> Integer.toString(x);
+        
+        log.info("{}", convertidor2.apply(3).length());
+        log.info("{}", convertidor2.apply(30).length());
+        log.info("{}", convertidor2.apply(300).length());
     }
 }
