03c2c6ea8fa09f1d5c9d815f47a5b107604c68f2
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-05 11:25:24 +0200

    update

--- InterfazFunctionTest/src/main/java/ejerciciofunction/TestFunction.java
+++ InterfazFunctionTest/src/main/java/ejerciciofunction/TestFunction.java
@@ -2,10 +2,14 @@ package ejerciciofunction;
 
 import java.util.function.Function;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TestFunction {
     public static void main(String[] args) {
         Function<Integer, String> convertidor = x -> Integer.toString(x);
-        System.out.println(convertidor.apply(3).length());
-        System.out.println(convertidor.apply(30).length());
+        
+        log.info("{}", convertidor.apply(3).length());
+        log.info("{}", convertidor.apply(30).length());
     }
 }
