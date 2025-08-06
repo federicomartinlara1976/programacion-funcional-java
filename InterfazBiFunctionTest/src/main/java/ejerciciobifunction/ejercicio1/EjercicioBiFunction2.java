f1853210ece6f6eb5b366f41f9badebd7da321c6
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-06 10:06:04 +0200

    Format code

--- InterfazBiFunctionTest/src/main/java/ejerciciobifunction/ejercicio1/EjercicioBiFunction2.java
+++ InterfazBiFunctionTest/src/main/java/ejerciciobifunction/ejercicio1/EjercicioBiFunction2.java
@@ -7,7 +7,7 @@ public class EjercicioBiFunction2 {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
         
-        String resultado = calc.calc((x, y) -> ": " + (x * y), 60, 10);
+        String resultado = calc.calc((x, y) -> "" + (x * y), 60, 10);
         
         log.info("El resultado de la multiplicacion es: {}", resultado);
     }
