9d712c4ad061c744fe89a47e27c3d737b8857274
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-06 21:34:41 +0200

    update

--- InterfazSupplierTest/src/main/java/ejercicios/SupplierTest.java
+++ InterfazSupplierTest/src/main/java/ejercicios/SupplierTest.java
@@ -1,6 +1,9 @@
 package ejercicios;
 import java.util.function.Supplier;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SupplierTest {
     public static void main(String[] args) {
         Supplier<String> i = () -> "Javier";
