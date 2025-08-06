e75e7663bcc2c84521aecaf3610e69e1e465ce75
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-06 10:54:29 +0200

    Optimización

--- InterfazPredicateTest/src/main/java/evaluaempleados_predicate_bifunction/Evaluador.java
+++ InterfazPredicateTest/src/main/java/evaluaempleados_predicate_bifunction/Evaluador.java
@@ -1,27 +1,16 @@
 package evaluaempleados_predicate_bifunction;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Evaluador {
-    public List<Empleado> evaluar(List<Empleado> listEmp, Predicate<Empleado> eval) {
-        List<Empleado> listaNueva = new ArrayList<>();
-        for (Empleado empleado : listEmp) {
-            if (eval.test(empleado)) {
-                listaNueva.add(empleado);
-            }
-        }
-        return listaNueva;
+    
+	public List<Empleado> evaluar(List<Empleado> listEmp, Predicate<Empleado> eval) {
+        return listEmp.stream().filter(eval).collect(Collectors.toList());
     }
 
     public List<Empleado> evaluarAlContrario(List<Empleado> listEmp, Predicate<Empleado> eval) {
-        List<Empleado> listaNueva = new ArrayList<>();
-        for (Empleado empleado : listEmp) {
-            if (eval.negate().test(empleado)) {
-                listaNueva.add(empleado);
-            }
-        }
-        return listaNueva;
+        return listEmp.stream().filter(eval.negate()).collect(Collectors.toList());
     }
 }
