dc59274e96eb72cc67212e824783a76ebe40226d
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-06 10:04:57 +0200

    Format code

--- InterfazBiFunctionTest/src/main/java/ejerciciobifunction/ejercicio1/Calculadora.java
+++ InterfazBiFunctionTest/src/main/java/ejerciciobifunction/ejercicio1/Calculadora.java
@@ -3,7 +3,8 @@ package ejerciciobifunction.ejercicio1;
 import java.util.function.BiFunction;
 
 public class Calculadora {
-    public String calc(BiFunction<Integer, Integer, String> bi, Integer i1, Integer i2) {
+    
+	public String calc(BiFunction<Integer, Integer, String> bi, Integer i1, Integer i2) {
         return bi.apply(i1, i2);
     }
 }
