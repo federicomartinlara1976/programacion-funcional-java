53b9035119f3fb9b0c5a6c03a38fefdddd3880f8
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-06 21:37:07 +0200

    update log

--- InterfazSupplierTest/src/main/java/ejercicios/SupplierTest.java
+++ InterfazSupplierTest/src/main/java/ejercicios/SupplierTest.java
@@ -7,6 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 public class SupplierTest {
     public static void main(String[] args) {
         Supplier<String> i = () -> "Javier";
-        log.info("El valor pasado es: " + i.get());
+        log.info("El valor pasado es: {}", i.get());
     }
 }
