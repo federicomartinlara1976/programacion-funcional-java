7813e0dbbfe0e714da63feeb62423c3c9cd3e5ff
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-06 21:43:49 +0200

    Update

--- InterfazUnaryOperator/src/main/java/UnaryOperatorTest.java
+++ InterfazUnaryOperator/src/main/java/UnaryOperatorTest.java
@@ -1,14 +1,17 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.function.UnaryOperator;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class UnaryOperatorTest {
     public static void main(String[] args) {
         List<Integer> lista = Arrays.asList(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
         List<Integer> listaAct = operadorUnary(num -> num * num, lista);
 
-        listaAct.forEach(x -> log.info(x));
+        listaAct.forEach(x -> log.info("{}", x));
     }
 
     public static List<Integer> operadorUnary(UnaryOperator<Integer> unaryOpt, List<Integer> lista) {
