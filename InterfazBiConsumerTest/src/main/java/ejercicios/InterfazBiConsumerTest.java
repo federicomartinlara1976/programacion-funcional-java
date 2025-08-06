1c34e3feccdf824266367930509a08a91e9fa77e
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-06 21:24:43 +0200

    Change type

--- InterfazBiConsumerTest/src/main/java/ejercicios/InterfazBiConsumerTest.java
+++ InterfazBiConsumerTest/src/main/java/ejercicios/InterfazBiConsumerTest.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 public class InterfazBiConsumerTest {
     public static void main(String[] args) {
         BiConsumer<Double, Double> biConsumer = (num1, num2) -> {
-            double mult = num1 * num2;
+            Double mult = num1 * num2;
             log.info("La multiplicación de {} y {} es {}", num1, num2, mult);
         };
         biConsumer.accept(15.2, 52.25);
