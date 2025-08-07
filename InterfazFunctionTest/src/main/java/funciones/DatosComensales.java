0b9de800570a867c95e9f9a185360d5a9a014427
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-07 11:49:33 +0200

    Remove unused import

--- InterfazFunctionTest/src/main/java/funciones/DatosComensales.java
+++ InterfazFunctionTest/src/main/java/funciones/DatosComensales.java
@@ -3,7 +3,6 @@ package funciones;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import lombok.extern.slf4j.Slf4j;
 import pojos.Comensal;
