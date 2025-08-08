6fdda9019658f8465fb361e01b1931d777546ea5
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-08 21:39:45 +0200

    Add test

---
+++ StreamTest/src/main/java/StreamTest.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StreamTest {
+
+	public static void main(String[] args) {
+		List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+		
+		Stream<Integer> numStream = numeros.stream();
+		
+		int suma = numStream
+				.filter(n -> n % 2 == 1) // Se obtiene {1, 3, 5}
+				.map(n -> n * n) // Se obtiene {1, 9, 25}
+				.reduce(0, (n1, n2) -> n1 + n2);
+		
+		log.info("La suma de los números impares es {}", suma);
+	}
+}
