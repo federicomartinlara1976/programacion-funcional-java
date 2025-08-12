56a36b4681f40104b24b130f5850baf566ce82e4
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-12 10:37:27 +0200

    update log

--- creacion_streams/src/main/java/ej_streams/CrearStream5.java
+++ creacion_streams/src/main/java/ej_streams/CrearStream5.java
@@ -15,12 +15,7 @@ public class CrearStream5 {
         Path path1 = Paths.get("/home/federico/.init/.bash_aliases");
 
         try (Stream<String> lineas = Files.lines(path1)) {
-            lineas.forEach(
-                linea -> {
-                    log.info("Linea...");
-                    log.info(linea);
-                }
-            );
+            lineas.forEach(linea -> log.info(linea));
         } catch (IOException e) {
             log.error("ERROR", e);
         }
