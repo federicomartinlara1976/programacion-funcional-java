2c7ca8e48530a66a4d20319a7663941aac545457
Federico Martín Lara <federicomartinlara1976@gmail.com>
2025-08-12 11:14:00 +0200

    Uncomment samples

--- OperacionesMap/src/main/java/operacionesmap/OperacionesMap.java
+++ OperacionesMap/src/main/java/operacionesmap/OperacionesMap.java
@@ -1,7 +1,9 @@
 package operacionesmap;
 
 import java.util.List;
+import java.util.function.IntUnaryOperator;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 import datos.Empleado;
 import lombok.extern.slf4j.Slf4j;
@@ -10,19 +12,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class OperacionesMap {
     public static void main(String[] args) {
-        /*IntStream.rangeClosed(1, 5)
+        IntStream.rangeClosed(1, 5)
             .map(new IntUnaryOperator() {
                 @Override
                 public int applyAsInt(int numero) {
                     return numero * numero;
                 }
             })
-            .forEach(System.out::println); //1 4 9 16 25
+            .forEach(n -> log.info("{}", n)); //1 4 9 16 25
 
         IntStream.rangeClosed(1, 10)
             .map(n -> n * 2)
-            .forEach(System.out::println); //2 4 6 8 10 12 14 16 18 20
-        */
+            .forEach(n -> log.info("{}", n)); //2 4 6 8 10 12 14 16 18 20
+        
         List<Empleado> empleados = Empleado.empleados();
 
         log.info("Promedio de ingresos de personal femenino mayor de 25 años: ");
