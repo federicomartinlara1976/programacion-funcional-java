05d54feef7ff537834b11c2eac809148aaf1f682
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-05-06 15:58:10 +0200

    Supplier especializado

--- InterfazSupplierTest/src/main/java/ejercicios/SupplierTest2.java
+++ InterfazSupplierTest/src/main/java/ejercicios/SupplierTest2.java
@@ -1,14 +1,14 @@
 package ejercicios;
-import java.util.function.Supplier;
+import java.util.function.DoubleSupplier;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SupplierTest2 {
     public static void main(String[] args) {
-        Supplier<Double> randomSupplier = () -> Math.random();
-        double value1 = randomSupplier.get();
-        double value2 = randomSupplier.get();
+        DoubleSupplier randomSupplier = Math::random;
+        double value1 = randomSupplier.getAsDouble();
+        double value2 = randomSupplier.getAsDouble();
         log.info("{} == {}: {}", value1, value2, value1 == value2);
     }
 }
