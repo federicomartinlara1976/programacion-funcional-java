365eeb3f49293b44a1639ff3b6cb74ff2c5d32c1
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-06-09 16:52:16 +0200

    Se convierte main a test

--- InterfazSupplierTest/src/main/java/ejercicios/SupplierTest2.java
+++ InterfazSupplierTest/src/main/java/ejercicios/SupplierTest2.java
@@ -1,14 +1,24 @@
 package ejercicios;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import java.util.function.DoubleSupplier;
+
+import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SupplierTest2 {
-    public static void main(String[] args) {
+	
+	@Test
+    void test() {
         DoubleSupplier randomSupplier = Math::random;
-        double value1 = randomSupplier.getAsDouble();
-        double value2 = randomSupplier.getAsDouble();
+        
+        Double value1 = randomSupplier.getAsDouble();
+        Double value2 = randomSupplier.getAsDouble();
+        
         log.info("{} == {}: {}", value1, value2, value1 == value2);
+        
+        assertNotEquals(value1, value2);
     }
 }
