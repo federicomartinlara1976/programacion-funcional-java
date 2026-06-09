5499c37a24738d75c93e94c8d84b39eb049903cd
Federico Martín Lara <federicomartinlara1976@gmail.com>
2026-06-09 16:53:59 +0200

    Convertido a test

--- InterfazSupplierTest/src/main/java/ejercicios/SupplierTest.java
+++ InterfazSupplierTest/src/main/java/ejercicios/SupplierTest.java
@@ -1,12 +1,21 @@
 package ejercicios;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.function.Supplier;
+
+import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SupplierTest {
-    public static void main(String[] args) {
+    
+	@Test
+	void test() {
         Supplier<String> i = () -> "Javier";
+        
         log.info("El valor pasado es: {}", i.get());
+        
+        assertEquals("Javier", i.get());
     }
 }
